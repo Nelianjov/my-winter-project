a009bd6576db73c2937a240fb7f538805d38ed97
Nelianjov <phirineliyamercy@gmail.com>
2024-01-27 20:34:14 +0300

    a window

--- snakegame.py
+++ snakegame.py
@@ -1,2 +1,12 @@
-print('hello world')
-print("written from his branch")
+import pygame
+pygame.init()
+width = 500
+height = 500
+green = (0,150,0)
+blue = (0,0,255)
+screen = pygame.display.set_mode((width, height))
+running = True
+while running:
+    screen.fill((green))
+    pygame.display.update()
+pygame.quit()
