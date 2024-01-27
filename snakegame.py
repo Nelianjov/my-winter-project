7665ad063cb5ec7675a41949729a08dc26530f10
Rckxa <chandack24@gmail.com>
2024-01-27 20:53:41 +0300

    added title to top of window

--- snakegame.py
+++ snakegame.py
@@ -1,10 +1,12 @@
 import pygame
 pygame.init()
+pygame.display.set_caption("Winter Project") # sets the title at the top of the window
 width = 500
 height = 500
 green = (0,150,0)
 blue = (0,0,255)
 screen = pygame.display.set_mode((width, height))
+
 running = True
 while running:
     for event in pygame.event.get():
