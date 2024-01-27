36b3eff59623b4209efb8ab3fd943dd45c3e1eba
Nelianjov <phirineliyamercy@gmail.com>
2024-01-27 20:43:57 +0300

    a window

--- snakegame.py
+++ snakegame.py
@@ -7,6 +7,9 @@ blue = (0,0,255)
 screen = pygame.display.set_mode((width, height))
 running = True
 while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+             running = False
     screen.fill((green))
     pygame.display.update()
 pygame.quit()
