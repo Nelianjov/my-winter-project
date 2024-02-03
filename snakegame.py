b522c91e22cf61e0e3366b96590f3d2506bc9c59
Nelianjov <phirineliyamercy@gmail.com>
2024-02-03 21:15:18 +0300

    merged code

--- snakegame.py
+++ snakegame.py
@@ -1,24 +1,100 @@
 import pygame
 import random
+
 pygame.init()
+pygame.display.set_caption("Winter Project")   # sets the title at the top of the window
+
 width = 500
 height = 500
-sides = 10 # the sides of rectangle
-green = (0,150,0)
-blue = (0,0,255)
-x = 250
-y = 250
+
+
+# COLOURS
+red = (255, 0, 0)
+green = (0, 150, 0)
+blue = (0, 0, 255)
+
 screen = pygame.display.set_mode((width, height))
+
+# coordinates of the snake
+snake_x = 0
+snake_y = 0
+sides = 10  # the sides of rectangle
+
+# coordinates of the fruit
+fruit_x = 250
+fruit_y = 250
+
+# velocity/direction of the snake
+vel_x = 10
+vel_y = 0
+
+
+clock = pygame.time.Clock()
+
+# beginning of the infinite loop
+
+
 running = True
 while running:
+
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
-             running = False
+            running = False
+
+
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_d:#generating the rect at a random position when you click d
-                x = random.randint(0, 500)
-                y = random.randint(0, 500)
-    screen.fill((green))
-    pygame.draw.rect(screen, (blue), pygame.Rect(x, y, sides,sides))# drawing the rect
+            if event.key == pygame.K_d:  # generating the rect at a random position when you click d
+                fruit_x = random.randint(0, width)
+                fruit_y = random.randint(0, height)
+
+
+# events for left and right
+            if event.key == pygame.K_RIGHT:
+                if snake_x <= width - sides:
+                    if not (vel_y == 0):
+                        if vel_x < 0:  # if the direction is negative such that to the left, make it go to the right
+                            vel_x = -vel_x
+                        else:
+                            vel_x = 10  # if the direction is positive such that to the right, leave it the way it is
+                            # pass just means do nothing
+                        vel_y = 0
+            if event.key == pygame.K_LEFT:
+                if not (vel_y == 0):
+                    if snake_x >= 0:
+                        if vel_x > 0:  # if direction is positive ie going to the right, then make
+                            # #it negative to go to the left
+                            vel_x = -vel_x
+                        else:
+                            vel_x = -10  # if the direction is negative already ie going to the left, do nothing
+                        vel_y = 0
+
+                # events for up and down
+            if not (vel_x == 0):
+                if event.key == pygame.K_UP:
+                    if snake_y >= 0:
+                        if vel_y > 0:
+                            vel_y = -vel_y
+                        else:
+                            vel_y = -10
+                        vel_x = 0
+                if event.key == pygame.K_DOWN:
+                    if snake_y <= height - sides:
+                        if vel_y < 0:
+                            vel_y = -vel_y
+                        else:
+                            vel_y = 10
+                        vel_x = 0
+
+    snake_x += vel_x
+    snake_y += vel_y
+    clock.tick(10)
+    screen.fill(green)
+    fruit = pygame.draw.rect(screen, blue, pygame.Rect(fruit_x, fruit_y, sides, sides))
+    snake = pygame.draw.rect(screen, blue, pygame.Rect(snake_x, snake_y, sides, sides))
     pygame.display.update()
+
+    if snake_x < 0 or snake_x > width - sides or snake_y < 0 or snake_y > width - sides:
+        # game over
+        running = False
 pygame.quit()
